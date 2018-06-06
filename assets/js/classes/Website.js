35ea0b730a67f08281fec951ea50a613de76540a
prodepa <mateusprodepa>
2018-06-06 16:11:15 -0300

    ultimas mudanças na interface e interação

--- assets/js/classes/Website.js
+++ assets/js/classes/Website.js
@@ -6,9 +6,9 @@ class Website {
     this.url = url;
     this._ = [];
     this.logs = document.querySelector(`#${this.nome}`).children[2];
-    this.loader = this.logs.children[0];
-    this.closeBtn = this.logs.children[1];
-    this.list = this.logs.children[2];
+    this.closeBtn = this.logs.children[0];
+    this.list = this.logs.children[1];
+    this.btn = document.querySelector(`#${this.nome}`).children[1].children[1];
     this.active = false;
 
     this.closeBtn.addEventListener('click', () =>
@@ -16,23 +16,48 @@ class Website {
   }
 
   testarModulos() {
-    this.loader.style.display = "block";
-    this.logs.classList.add('active');
+    let n = 0;
+    const self = this;
+
+    const c = setInterval(function() {
+      n++;
+      if(n >= 10) {
+        document.querySelector('.error-messages').innerHTML = "O tempo para a verificação foi esgotado.";
+        document.querySelector('.error-messages').style.transform = 'translateY(0)';
+        self.btn.removeAttribute('disabled');
+        self.btn.innerHTML = 'Testar ambiente';
+
+        setTimeout(function() {
+          document.querySelector('.error-messages').style.transform = 'translateY(210%)';
+        }, 2000);
+      }
+    }, 1000);
+
+    this.active = true;
+    this.btn.innerHTML = `Testar ambiente<div class="logs__loader"></div>`;
+    this.btn.setAttribute('disabled', 'disabled');
     this.st = [];
     this.testes.forEach(teste =>
       $.post(this.url, { function: teste }, res => {
         this.st.push({ nome: teste, res });
-        this.st.length >= this.testes.length ? this.atualizarValores() : null;
+        if(this.st.length >= this.testes.length) {
+          clearInterval(c);
+           this.atualizarValores();
+        }
     }));
 
     this._.push(this.st);
   }
 
   atualizarValores() {
-    this.loader.style.display = "none";
+
+    this.active = false;
     this.list.innerHTML = "";
-    this._.forEach(e => e.forEach(t => {
+    this.st.forEach(t => {
       this.list.innerHTML += `<li>${t.res}</li>`;
-    }));
+    });
+    this.logs.classList.add('active');
+    this.btn.innerHTML = 'Testar ambiente';
+    this.btn.removeAttribute('disabled');
   }
 }
