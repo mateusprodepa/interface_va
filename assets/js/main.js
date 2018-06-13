99ba22c3cdf21c9aa1dfdf5e4fee91e3b523c67d
marcelo.cruz <marcelo.cruz@prodepa.pa.gov.br>
2018-06-13 13:54:00 -0300

    ultima versão

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let t = new Array();
 const wrapper = document.querySelector('.wrapper');
 const dadosBtn = document.querySelectorAll('.dadosSistema__button');
@@ -8,18 +10,19 @@ function generateBoxes() {
 
   boxes = new Array(c);
   for(var x = 0; x < boxes.length; x++) {
+    const r = Math.floor(Math.random() * 360);
     const n = Math.floor(Math.random() * (7 - 2) + 2);
     const top = Math.floor(Math.random() *
     window.getComputedStyle(wrapper, null)
     .getPropertyValue('height')
-    .match(/\d*/gi)[0]) - 70;
+    .match(/\d*/gi)[0]) - 120;
 
     const left = Math.floor(Math.random() *
     window.getComputedStyle(wrapper, null)
     .getPropertyValue('width')
-    .match(/\d*/gi)[0]) + 150;
+    .match(/\d*/gi)[0]) + 170;
 
-    wrapper.innerHTML += `<div style="top: ${top}px; left: ${left}px;" class='box box${n}'></div>`;
+    wrapper.innerHTML += `<div style="top: ${top}px; left: ${left}px; transform: rotate(${r}deg)" class='box box${n}'></div>`;
   }
 }
 
@@ -34,6 +37,5 @@ dadosBtn
     t.find(tes => btn.dataset.name === tes.nome).testarModulos();
   });
 });
-
 
 generateBoxes();
